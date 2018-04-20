89c1785014f1ea2cb1d8046b91264c9022d427a8
Stephanie Hatz <seh46@duke.edu>
2018-04-20 15:00:52 -0400

    Set up server to interface with separate api functions

--- webserver.py
+++ webserver.py
@@ -3,6 +3,10 @@ import models
 import datetime
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import decodeimage
+import logging
+import apifunctions as api
+import datetime
 
 connect("mongodb://vcm-3483.vm.duke.edu:27017/image_app")
 app = Flask(__name__)
@@ -14,54 +18,34 @@ def main_task():
 
     :return:
     """
+    s = request.get_json()
+    email = s["email"]
+    functions = s["functions"]
+    up_time = datetime.datetime.now()
     try:
-        # get image files
-        # timestamp
-        # convert files to storage format (Eitan function)
-    except:
-        # send frontend an error (or use status code???)
-    try:
-    # if user doesn't exist, create user and set metrics
-    # in user array, create new subarray for user
-    # subarray should contain dictionary of following keys:
-        # user email
-        # user metrics (function array)
-        # current timestamp
-    except:
-    # update metrics for existing user
-    # increment user metrics (function array)
-    # replace timestamp with current
-    try:
-        # store original file directories in database (using models? see old main.py)
-        # in image array, create new subarray for batch
-        # subarray should contain dictionary of following keys:
-            # user email
-            # original images (array of directories)
-            # processed images (blank for now)
-            # image metadata array with dictionary:
-                # up_time (current timestamp)
-                # functions (function array from frontend)
-    except:
-        # set db flag to 1
-    try:
-        # for loop for each image (or will Eitan have for loop?)
-            # process images (Eitan function)
-            # histograms of both images (Eitan function)
-            # image sizes (Eitan function)
-            # timestamp
-    except:
-        # send frontend an error (couldn't process images)
-    try:
-        # store processed file directories in database
-        # populate processed images key
-        # populate image metadata key with dictionary:
-            # original histogram
-            # processed histogram
-            # original image size
-            # processed image size
-            # ret_time (current timestamp)
-    except:
-        # set db flag to 1
-    try:
+        originals = s["originals"] # get image files
+        # verify that images are encoded in base64
+        try:
+            api.existing_user_metrics(email, functions) # update metrics for existing user
+        except:
+            api.new_user_metrics(email, functions) # if user doesn't exist, create user and set metrics
+        try:
+            # save images in local directory with UUID name
+            api.store_uploads(email, originals, up_time, functions)  # store original file directories in database
+        except:
+            logging.error('Could not save images to database')
+        try:
+            # for loop for each image (or will Eitan have for loop?)
+                # process images (Eitan function)
+                # histograms of both images (Eitan function)
+                # image sizes (Eitan function)
+                # timestamp
+            try:
+                api.store_returns(email, processed, o_histogram, p_histogram, o_size, p_size) # store processed images and data in database
+            except:
+                logging.error('Could not store processed images in database')
+        except:
+            logging.error('Could not process uploaded images')
         # jsonify all info in image array, user array, and flags
-        # flags (0 error, 1 pass):
+    except:
+        logging.error('Did not receive image encoded in base64') # Image not uploaded or encoded incorrectly
