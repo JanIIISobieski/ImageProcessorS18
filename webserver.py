c6c018e5da3f2c1ef56a95d29b75bca2195df83e
Stephanie Hatz <seh46@duke.edu>
2018-04-30 19:04:53 -0400

    Add gunicorn debugging block to file

--- webserver.py
+++ webserver.py
@@ -14,6 +14,12 @@ app = Flask(__name__)
 CORS(app)
 
 
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+
+
 @app.route("/api/process_images", methods=["POST"])
 def main_task():
     """
