e6dda20d17e462401ecf3a802dc486ca1aa98b3e
Stephanie Hatz <seh46@duke.edu>
2018-05-01 12:59:33 -0400

    Change email field to a char field to allow for non-email usernames

--- models.py
+++ models.py
@@ -2,13 +2,13 @@ from pymodm import fields, MongoModel
 
 
 class User(MongoModel):
-    email = fields.EmailField(primary_key=True)
+    email = fields.CharField(primary_key=True)
     metrics = fields.ListField(fields.IntegerField())
     time = fields.DateTimeField()
 
 
 class ImageBatch(MongoModel):
-    email = fields.EmailField(primary_key=True)
+    email = fields.CharField(primary_key=True)
     o_image = fields.ListField(fields.CharField())
     p_image = fields.ListField(fields.CharField())
     up_time = fields.DateTimeField()
