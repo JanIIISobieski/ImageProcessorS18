30342c3da6397bc8df4dbd9056a197843cde7240
Stephanie Hatz <seh46@duke.edu>
2018-05-01 19:32:12 -0400

    Try retrieving all image batches for given user and see what is output

--- apifunctions.py
+++ apifunctions.py
@@ -79,8 +79,8 @@ def get_latest_batch(email):
     :param email: username input (string)
     :return: image batch object from database
     """
-    a = models.ImageBatch.objects.get({"_id": email}).\
-        order_by([(models.ImageBatch.ret_time, 'descending')])
+    a = models.ImageBatch.objects.get({"_id": email}).all()
+    import pytest; pytest.set_trace()
     b = a.first()
     return b
 
